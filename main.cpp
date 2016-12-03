001fa16e9b72727a5573c0e2f54d01a721db4c92
sardap <pfsarda23@gmail.com>
2016-12-03 13:38:51 -0500

    re-wrote every line created classes can read docmnet and create tokkens
    need  to now make it do somthing with the info

--- main.cpp
+++ main.cpp
@@ -1,90 +1,283 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <regex>
+#include <vector>
+#include <map>
+#define NUM_OF_OPS 6
 
 using namespace std;
 
-typedef struct{
-	int length;
-	string *value;
-} stringArray;
-
-typedef struct {
-	string inFileName;
-	string outFileName;
-	stringArray code;
-} intr_options;
-
-int get_num_line(string inFileName){
-	ifstream file(inFileName.c_str());
-	int numLine = 0;
-	string temp;
-	while(getline(file, temp)){
-		numLine++;
-	}
-	file.close();
-	return numLine;
+bool is_number(const string& str){
+	return !str.empty() && find_if(str.begin(), str.end(), [](char ch) { return !isdigit(ch); }) == str.end();
 }
 
-int muhterpret(intr_options *options){
-	ofstream outFile(options->outFileName.c_str());
+std::string random_insult(int strength){
+	std::string insult = "you";
+	std::string temp = "";
+	int x;
 	int i;
-	if(outFile.is_open()){
-		for(i=0; i < options->code.length; i++){
-			cout << "muhterpreting " << options->code.value[i] << "\n";
-		}
+	//makes insult length is strength
+	for(i=0; i<strength; i++){
+		// dowhile loop used to ensure there are no doubles in the insult
+		do{
+			x = rand() % 12;
+			switch (x) {
+				case 0:  temp = " Fucking";   break;		case 5:  temp = " Trump"; break;
+				case 1:  temp = " enchilada"; break;		case 6:  temp = " Idiot"; 				break;
+				case 2:  temp = " Jerry"; 		break;		case 7:  temp = " Piss head"; 		break;
+				case 3:  temp = " Fattie"; 		break;		case 8:  temp = " Cunt"; 					break;
+				case 4:  temp = " Stupid"; 		break;		case 9:  temp = " Shithead"; 			break;
+				case 10: temp = " Drugoo"; 		break;		case 11: temp = " Captalist Pig"; break;
+			}
+		}while(insult.find(temp) != std::string::npos);
+		insult += temp;
 	}
-	return 1;
+	return insult;
 }
-
-int read_file(intr_options *options){
-	int numLine = get_num_line(options->inFileName);
-	ifstream codeFile(options->inFileName.c_str());
-	int i;
-	string line;
-	if (codeFile.is_open()){
-		cout << "File Open \n";
-		options->code.length = numLine;
-		options->code.value = new string[options->code.length];
-		for(i=0; getline(codeFile, line);i++){
-			options->code.value[i] = line;
+//each word is a me_word
+class me_word {
+	public:
+		//stores the stirng
+		std::string word;
+		me_word(std::string str, int wordNumAss, int cellNumAss){
+			word = str;
+			wordNum = wordNumAss;
+			cellNum = cellNumAss;
+			//checks if word is a number
+			if(is_number(str)){
+				action = "num";
+			} else {
+				action = get_action();
+			}
 		}
-		codeFile.close();
-		return 1;
+		std::string info(){
+			std::stringstream result;
+			result << "WORD:" << word << " NUM:" << wordNum << " CELLNUM:" << cellNum << " ACTION:" << action;
+			return result.str();
+		}
+		void set_end(){
+			action = "END";
+		}
+	private:
+		int wordNum;
+		int cellNum;
+		std::string action;
+		std::string get_action(){
+			std::string str;
+			//based on word and wordNumber retruns the apporite tokken
+			if(word == "me"){
+				str = "ary["+to_string(cellNum)+"]";
+			} else {
+				switch (wordNum) {
+					case 0: str = "="; break;		case 1: str = "+"; break;
+					case 2: str = "-"; break;		case 3: str = "*"; break;
+					case 4: str = "/"; break;		case 5: str = "%"; break;
+					case 6: str = "printf(\"%c\", ary[" + to_string(cellNum) + "]);\n"; break;
+				}
+			}
+			return str;
+		}
+};
+//each line contains many words
+class me_line {
+	public:
+		me_line(std::string str, int assLineNum){
+			line = str;
+			lineNum = assLineNum;
+			//onlt matchs muh me and numbers
+			std::regex phrase ("(muh)|(me)|([0-9][^ ]+)");
+			int wordNum = 0;
+			int cellNum = 0;
+			std::regex_search(line.c_str(), phrase);
+			//Loops though matchs
+			for(std::sregex_iterator i = std::sregex_iterator(line.begin(), line.end(), phrase);
+			i != std::sregex_iterator(); ++i){
+				std::smatch match = *i;
+				//creates a word with the appotie wordNum and cellNum
+				me_word tempWord(match.str(), wordNum, cellNum);
+				words.push_back(tempWord);
+				//Increments CellNum and or wordNum apporitely
+				if(match.str() == "me"){
+					wordNum = 0;
+					cellNum++;
+				} else {
+					if(wordNum>NUM_OF_OPS){
+						wordNum = 0;
+					} else {
+						wordNum++;
+					}
+				}
+			}
+			std::cout << "Cleaned String" << '\n';
+			//Not sure how to handle erros :/
+			if(!clean_string()){
+				// exit(EXIT_FAILURE);
+			} else {
+				//prints all of the words in the line
+				for(auto element : words){
+					std::cout << element.info() << "\n";
+				}
+				// std::cout << '\n';
+			}
+		};
+	private:
+		std::string line;
+		int lineNum;
+		std::vector<me_word> words;
+		int clean_string(){
+			unsigned int i;
+			if(words.size() >= 2){
+				std::cout << "size: "<< words.size() << '\n';
+				//checks that the first word is me
+				if(words[0].word != "me"){
+					std::cout << "ERROR@ln " << lineNum << ": MISSING STARTING ME " << random_insult(rand()%5+2) <<'\n';
+					return 0;
+				}
+				//removes all but the last word
+				for(i=0; i<words.size()-1; /*std::cout << i << "<" << words.size()-2 << '\n'*/){
+					// std::cout << "words[" << i << "]" << words[i].word << "== words[" << i << "]" << words[i+1].word  <<  '\n';
+					if(words[i].word == words[i+1].word){
+						// std::cout << "Delteing " << i << "\n";
+						words.erase(words.begin()+i);
+						i=0;
+					} else {
+						i++;
+					}
+				}
+				//checks that the last word is muh then sets to END
+				if(words[i].word == "muh"){
+					words[i].set_end();
+				} else {
+					std::cout << "ERROR@ln " << lineNum << ": MISSING ENDING MUH " << random_insult(5) <<'\n';
+					return 0;
+				}
+			} else {
+				std::cout << "ERROR@ln" << lineNum << ": UNDER TWO WORDS " << random_insult(3) << '\n';
+				return 0;
+			}
+			return 1;
+		}
+};
+
+class me_code {
+	public:
+		//adds a line to the code
+		int add_line(string line){
+			me_line tempLine(line, lineNum);
+			lineNum++;
+			lines.push_back(tempLine);
+			return 1;
+		}
+		me_code(){
+			lineNum = 1;
+		}
+	private:
+		int lineNum;
+		std::vector<me_line> lines;
+};
+
+class me_file {
+	public:
+		std::string name;
+		std::ifstream file;
+		//returns number of lines in file
+		int lines(){
+			int numLine = 0;
+			if (file.is_open()){
+				file.seekg (0, file.beg);
+				std::string temp;
+				while(std::getline(file, temp)){
+					numLine++;
+				}
+				file.clear();
+				file.seekg (0, std::ios::beg);
+				return numLine;
+			}
+			return 0;
+		};
+		//assigns a name to the file with the apptorite File ending
+		int assign_name(string str){
+			switch(type){
+				case 'i':
+					name = str;
+					break;
+				case 'o':
+					if (str.find(".\\") != string::npos){
+						str = str.substr(2, str.length());
+					}
+					if (str.find(".") != string::npos){
+						name = str.substr(0, str.find("."));
+					} else {
+						name = str;
+					}
+					name += ".c";
+
+			}
+			std::cout << name << '\n';
+			return 1;
+		}
+		//opens the file
+		int open(){
+			file.open(name.c_str());
+			if(file.is_open()){
+				return 1;
+			}
+			cout << "Unable to open " << name << endl;
+			return 0;
+		}
+		//checks if open
+		int is_open(){
+			if(file.is_open()){
+				return 1;
+			}
+			return 0;
+		}
+		me_file(char typeAss): type(typeAss) {};
+	private:
+		char type;
+};
+
+class ca_intr_options {
+	public:
+		me_file inFile {'i'};
+		me_file outFile {'o'};
+		me_code code;
+		int read_file(){
+			std::string line;
+			std::string::size_type sz;
+			while(std::getline(inFile.file, line)){
+				std::cout << '\n' << line << '\n';
+				code.add_line(line);
+			}
+			return 0;
+		}
+};
+
+int get_parma(int argc, char *argv[], ca_intr_options *options){
+	string strTemp;
+	int i=1;
+	if (argc >= 2){
+		options->inFile.assign_name(argv[i]);
+		if (argc >= 3){
+			i++;
+		}
+		options->outFile.assign_name(argv[i]);
+		options->inFile.open();
+		return options->inFile.is_open();
+	} else {
+		cout << "must select input file" << endl;
 	}
-	cout << "File Could not be opend \n";
 	return 0;
 }
 
-int get_parma(int argc, char *argv[], intr_options *options){
-	if (argc >= 2){
-		options->inFileName = argv[1];
-		if (argc == 3){
-			options->outFileName = argv[2];
-		} else
-			if (options->inFileName.find(".") != string::npos){
-				options->outFileName = options->inFileName.substr(0, options->inFileName.find("."));
-			} else {
-				options->outFileName = options->inFileName;
-			}
-			options->outFileName = options->outFileName + ".exe";
-		return 1;
-	} else{
-		return 0;
-	}
-}
-
 int main(int argc, char *argv[]){
-	intr_options options;
+	srand (time(NULL));
+	ca_intr_options options;
 	if (get_parma(argc, argv, &options)){
-		cout << "input File " << options.inFileName << " Output File " << options.outFileName << "\n";
-		if (read_file(&options)){
-			cout << "File Read " << options.code.length << " lines" << "\n";
-			muhterpret(&options);
-		}
-	} else {
-		cout << "must select Input File \n";
+		cout << "reading " << options.inFile.name << " with a total of " << options.inFile.lines() << " Lines " << endl;
+		options.read_file();
 	}
+
 	return 0;
 }
